20af732d35250cb9c4a10de14d66fc22d37f8dab
ilwel <ilwelisaac@hotmail.com>
2021-08-12 14:54:47 -0300

    PasswordInput component

---
+++ src/components/PasswordInput/index.js
@@ -0,0 +1,50 @@
+import { useState } from 'react';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
+import FormControl from '@material-ui/core/FormControl';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import IconButton from '@material-ui/core/IconButton';
+import useStyles from './styles';
+
+export default function PasswordInput({label}) {
+
+    const classes = useStyles();
+
+    const [values, setValues] = useState({
+        showPassword: false,
+    });
+
+    const handleClickShowPassword = () => {
+        setValues({ ...values, showPassword: !values.showPassword });
+    };
+
+    const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+    };
+
+    return (
+
+        <FormControl className={classes.root}>
+            <InputLabel htmlFor="standard-adornment-password">{label}</InputLabel>
+            <Input
+                id="standard-adornment-password"
+                type={values.showPassword ? 'text' : 'password'}
+                value={values.password}
+                endAdornment={
+                    <InputAdornment position="end">
+                        <IconButton
+                            aria-label="toggle password visibility"
+                            onClick={handleClickShowPassword}
+                            onMouseDown={handleMouseDownPassword}
+                        >
+                            {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                        </IconButton>
+                    </InputAdornment>
+                }
+            ></Input>
+        </FormControl>
+
+    )
+}
