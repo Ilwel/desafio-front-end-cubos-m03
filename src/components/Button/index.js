81c2ac58fa5d914c5d251af25c2da09bd72b8222
ilwel <ilwelisaac@hotmail.com>
2021-08-12 21:45:34 -0300

    Button onClick

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,9 +1,9 @@
 import Button from '@material-ui/core/Button';
 
-export default function CustomButtom({ variant, color, type ,content }) {
+export default function CustomButtom({ variant, color, type ,content, onClick }) {
 
     return (
-        <Button variant={variant} color={color} type={type}>
+        <Button variant={variant} color={color} type={type} onClick={onClick}>
             {content}
         </Button>
     )
